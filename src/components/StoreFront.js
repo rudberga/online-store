8e658b2aec735c8ec3c6ecf3d149e2512830b6c7
alrud5 <rudberg@pm.me>
2022-10-18 10:22:50 +0200

    ADD - components

---
+++ src/components/StoreFront.js
@@ -0,0 +1,16 @@
+export default function StoreFront() {
+    // hardcoded products for this project
+    const products = [{
+        name: "Cheese",
+        description: "200g cheese block",
+        image: "https://res.cloudinary.com/dbfn5lnvx/image/upload/q_auto,w_300/v1580649404/react-tutorial/products/cheese.png"
+    }, {
+        name: "Milk",
+        description: "1L of milk",
+        image: "https://res.cloudinary.com/dbfn5lnvx/image/upload/q_auto,w_300/v1580649400/react-tutorial/products/milk.png"
+    }]
+    return <div className="store-front">
+        {/* render the two Products here */}
+
+    </div>;
+};
